149fd4541adb67f364bc7d77669e2fb2a9fa33b0
Janek <eunho.moon@gmail.com>
2023-06-28 23:02:48 +0900

    api-gateway custom filter

--- apigateway-service/src/main/java/com/janek/apigatewayservice/config/FilterConfig.java
+++ apigateway-service/src/main/java/com/janek/apigatewayservice/config/FilterConfig.java
@@ -8,20 +8,20 @@ import org.springframework.context.annotation.Configuration;
 //@Configuration
 public class FilterConfig {
 
-//    @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(route -> route.path("/first-service/**")
-                        .filters(filter -> filter
-                                .addRequestHeader("first-request", "first-request-header")
-                                .addResponseHeader("first-response", "first-response-header"))
-                        .uri("http://localhost:8081"))
-                .route(route -> route.path("/second-service/**")
-                        .filters(filter -> filter
-                                .addRequestHeader("second-request", "second-request-header")
-                                .addResponseHeader("second-response", "second-response-header"))
-                        .uri("http://localhost:8082"))
-                .build();
-    }
+  //    @Bean
+  public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+    return builder.routes()
+        .route(route -> route.path("/first-service/**")
+            .filters(filter -> filter
+                .addRequestHeader("first-request", "first-request-header")
+                .addResponseHeader("first-response", "first-response-header"))
+            .uri("http://localhost:8081"))
+        .route(route -> route.path("/second-service/**")
+            .filters(filter -> filter
+                .addRequestHeader("second-request", "second-request-header")
+                .addResponseHeader("second-response", "second-response-header"))
+            .uri("http://localhost:8082"))
+        .build();
+  }
 
 }
--- apigateway-service/src/main/java/com/janek/apigatewayservice/filter/CustomFilter.java
+++ apigateway-service/src/main/java/com/janek/apigatewayservice/filter/CustomFilter.java
@@ -11,27 +11,27 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @Component
 public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
-    public CustomFilter() {
-        super(Config.class);
-    }
+  public CustomFilter() {
+    super(Config.class);
+  }
 
-    @Override
-    public GatewayFilter apply(Config config) {
-        return ((exchange, chain) -> {
-            ServerHttpRequest request = exchange.getRequest();
-            ServerHttpResponse response = exchange.getResponse();
+  @Override
+  public GatewayFilter apply(Config config) {
+    return ((exchange, chain) -> {
+      ServerHttpRequest request = exchange.getRequest();
+      ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Custom PRE filter: request id -> {}", request.getId());
+      log.info("Custom PRE filter: request id -> {}", request.getId());
 
-            return chain.filter(exchange)
-                    .then(Mono.fromRunnable(() -> {
-                        log.info("Custom POST filter: response code -> {}", response.getStatusCode());
-                    }));
-        });
-    }
+      return chain.filter(exchange)
+              .then(Mono.fromRunnable(() -> {
+                log.info("Custom POST filter: response code -> {}", response.getStatusCode());
+              }));
+    });
+  }
 
-    public static class Config {
-        // configuration properties
-    }
+  public static class Config {
+    // configuration properties
+  }
 
 }
